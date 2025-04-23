ced0852e67f6a325ef8b3d49778dbbc85d66f666
lily <hueduong288@gmail.com>
2025-04-23 03:51:17 +0000

    created and used _getenv manual function

--- path.c
+++ path.c
@@ -1,6 +1,30 @@
 #include "shell.h"
 
 /**
+* _getenv - built getenv manually
+* @name: name of the enviroment variable
+* Return: the pointer to the value, or NULL if not found
+*/
+
+char *_getenv(const char *name)
+{
+    int i = 0;
+    size_t name_len = strlen(name);
+
+    while (environ[i])
+    {
+        /* compare the start string of environ with given name */
+        if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
+        {
+            /* return pointer to value (after '=') */
+            return (&environ[i][name_len + 1]);
+        }
+        i++;
+    }
+    return (NULL); /* if not found */
+}
+
+/**
 * find_command_path - search PATH for an executable matching command
 * @cmd: the command to search for
 * Return: Full path to the command, or NULL if not found
@@ -21,7 +45,7 @@ char *find_command_path(char *cmd)
     }
 
     /* get PATH environment variable */
-    path_env = getenv("PATH");
+    path_env = _getenv("PATH");
     if (!path_env)
         return (NULL);
 
